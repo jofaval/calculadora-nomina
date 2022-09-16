73b8858e0a693ad0017669ee5a6f56469226f082
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-16 12:25:13 +0200

    hotfix(prod-deployment): Fixed base URL in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+
+const base =
+  process.env.NODE_ENV === "production" ? "/calculadora-nomina/" : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname),
+    },
+  },
+});
