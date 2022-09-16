723e2259f4689ad548d72ff63d105c9219c16b34
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-16 15:36:19 +0200

    fix: Translate footer

--- src/Navigation/Footer.tsx
+++ src/Navigation/Footer.tsx
@@ -3,10 +3,10 @@ import linksConstants from "../constants/links.constants";
 
 const Footer: React.FC = () => (
   <footer className="footer__container">
-    <div className="footer">
-      <p className="footer__title text-center">
-        Checkout my other repositories at{" "}
-        <a href={linksConstants.GITHUB} className="link">
+    <div className="footer my-4">
+      <p className="footer__title text-center text-white">
+        Échale un vistazo a mis otros proyectos/repositorios{" "}
+        <a href={linksConstants.GITHUB} className="text-bold">
           @github/jofaval
         </a>
       </p>
