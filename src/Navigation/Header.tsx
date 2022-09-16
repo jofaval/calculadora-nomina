f9e4b5677260b3853616a1298030b7a66e114bfa
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-16 15:30:27 +0200

    feat: Add margin to Header Title

--- src/Navigation/Header.tsx
+++ src/Navigation/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => (
   <header className="header__container">
     <div className="header">
-      <h1 className="header__title text-3xl font-bold text-center">
+      <h1 className="header__title text-3xl font-bold text-center mb-4">
         Calculadora de nóminas
       </h1>
     </div>
