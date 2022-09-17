da00d238d8ddc1ab9b54862e261f7e9f392572b9
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-17 12:12:45 +0200

    feat(footer); Changed background color

--- src/Navigation/Footer.tsx
+++ src/Navigation/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import linksConstants from "../constants/links.constants";
 
 const Footer: React.FC = () => (
-  <footer className="footer__container">
+  <footer className="footer__container bg-purple-600">
     <div className="footer my-4">
       <p className="footer__title text-center text-white">
         Échale un vistazo a mis otros proyectos/repositorios{" "}
