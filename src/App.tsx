7bb413a9c4e5d00a7fec43048960680f70f96018
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-16 21:27:22 +0100

    feat: Restyle the main hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,79 @@
+import { Disclosure } from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import React from "react";
 import { Footer, Header } from "./Navigation/";
 import { Payroll } from "./Payroll";
 
+const toggleDisclosureClass = (open: boolean): string =>
+  `${open ? "rotate-90 transform" : ""} h-5 w-5 text-purple-500`;
+
 const App: React.FC = () => (
-  <div className="app__container bg-gradient-to-br from-green-500 via-blue-500 to-cyan-500 w-full h-full">
-    <div className="app p-3 container mx-auto" id="app">
+  <div className="app__container bg-gradient-to-br from-green-500 via-blue-500 to-cyan-500 w-full">
+    <div
+      className="app p-3 container mx-auto min-h-screen flex flex-col justify-start align-items-center"
+      id="app"
+    >
       <nav className="app__header">
         <Header />
       </nav>
 
-      <main className="app__main">
-        <Payroll />
+      <main className="app__main rounded-2xl bg-white p-2 justify-self-stretch">
+        <Disclosure defaultOpen={true}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                Datos generales
+                <ChevronRightIcon className={toggleDisclosureClass(open)} />
+              </Disclosure.Button>
+
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                <Payroll />
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure defaultOpen={false}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                Datos familiares
+                <ChevronRightIcon className={toggleDisclosureClass(open)} />
+              </Disclosure.Button>
+
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                <Payroll />
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure defaultOpen={false}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                Discapacidades
+                <ChevronRightIcon className={toggleDisclosureClass(open)} />
+              </Disclosure.Button>
+
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                <Payroll />
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure defaultOpen={false}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                Datos de personas a cargo
+                <ChevronRightIcon className={toggleDisclosureClass(open)} />
+              </Disclosure.Button>
+
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                <Payroll />
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
       </main>
 
       <div className="app__footer">
--- src/Navigation/Footer.tsx
+++ src/Navigation/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import linksConstants from "../constants/links.constants";
 
 const Footer: React.FC = () => (
-  <footer className="footer__container bg-gradient-to-tl from-purple-600 to-cyan-400 absolute bottom-0 left-0 p-3 mt-5 rounded-t-lg w-full">
+  <footer className="footer__container bg-gradient-to-tl from-purple-600 to-cyan-400 p-3 mt-5 rounded-t-lg w-full">
     <div className="footer my-4">
       <p className="footer__title text-center text-white">
         Échale un vistazo a mis otros proyectos/repositorios{" "}
