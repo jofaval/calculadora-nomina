f2754397dd04332390d57ef67f86fdea2a5999e9
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-16 20:42:19 +0100

    chore: Install zustand, HeadlessUI and config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const base =
   process.env.NODE_ENV === "production" ? "/calculadora-nomina/" : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname),
-    },
-  },
 });
