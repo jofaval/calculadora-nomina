66a1532f5488b6732c8b621dc4bd142418e89b9d
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-19 14:22:37 +0100

    fix: Add missing imports indexation

---
+++ src/sections/index.ts
@@ -0,0 +1,4 @@
+export { Disability } from "./Disability/Disability.section";
+export { Family } from "./Family/Family.section";
+export { InCharge } from "./InCharge/InCharge.section";
+export { Overall } from "./Overall/Overall.section";
