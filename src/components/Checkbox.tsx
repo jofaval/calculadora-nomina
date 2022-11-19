2c82c4c17c1979a9874b96b6bd850f601fecb9e1
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-19 14:43:51 +0100

    feat(checkbox): Modify label order

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -29,6 +29,13 @@ export function Checkbox({
 
   return (
     <div className="flex items-center mb-4">
+      <label
+        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        htmlFor={fallbackId}
+      >
+        {label}
+      </label>
+      &nbsp;
       <input
         checked={isChecked}
         className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
@@ -37,12 +44,6 @@ export function Checkbox({
         onChange={onChangeHandle}
         type="checkbox"
       />
-      <label
-        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        htmlFor={fallbackId}
-      >
-        {label}
-      </label>
     </div>
   );
 }
